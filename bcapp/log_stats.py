20da809997c8efe0a1f9d8cc7cd656e1127ba58d
Max Pearl <maxwellbpearl@gmail.com>
2020-04-17 14:26:47 -0700

    Adding S3 read, bugfixes.

--- bcapp/log_stats.py
+++ bcapp/log_stats.py
@@ -6,12 +6,15 @@ version 0.2
 import sys
 import os
 import datetime
+import time
 import click
 import sh
 import logging
 from proxy_utilities import get_configs
 from simple_AWS.s3_functions import *
 from log_reporting_utilities import analyze_file, output
+
+logger = logging.getLogger('logger')
 
 @click.command()
 @click.option('--percent', type=int, help="Floor percentage to display for agents and codes (default is 5%)", default=5)
@@ -23,29 +26,64 @@ from log_reporting_utilities import analyze_file, output
 @click.option('--paths_ignore', type=str, help="Comma delimited list (no spaces) of paths to ignore for log analysis.")
 @click.option('--justsave', is_flag=True, default=False, help="Just save log files to S3, don't run any analysis.")
 @click.option('--read_s3', is_flag=True, default=False, help="Read logfiles from S3, not from local paths.")
+@click.option('--range', type=int, help="Days of log file age to analyze. Default is 10", default=10)
 
-def analyze(recursive, unzip, percent, num, daemon, skipsave, paths_ignore, justsave, read_s3):
+def analyze(recursive, unzip, percent, num, daemon, skipsave, paths_ignore, justsave, read_s3, range):
     configs = get_configs()
     now = datetime.datetime.now()
     now_string = now.strftime('%d-%b-%Y:%H:%M:%S')
+
+    s3simple = S3Simple(region_name=configs['region'],
+                                profile=configs['profile'],
+                                bucket_name=configs['log_storage_bucket'])
+
     # get the file list to analyze
-    if configs['paths']:
-        # open paths file
-        with open(configs['paths']) as pathfile:
-            raw_path_list = pathfile.read()
-        paths = raw_path_list.split('\n')
+    if not read_s3:
+        logger.debug("Reading Local Files...")
+        if configs['paths']:
+            # open paths file
+            with open(configs['paths']) as pathfile:
+                raw_path_list = pathfile.read()
+            paths = raw_path_list.split('\n')
+    else: # read from S3, and download to local tmp
+        logger.debug("Getting files from S3 bucket...")
+        file_list = s3simple.s3_bucket_contents()
+        logger.debug(f"File list: {file_list}")
+        paths = []
+        for ifile in file_list:
+            logger.debug(f"Processing file: {ifile}")
+            try:
+                (prefix, domain, date, filename) = ifile.split('_')
+            except ValueError:
+                continue
+            if prefix != 'RawLogFile':
+                continue
+            file_date = datetime.datetime.strptime(date, "%d-%b-%Y:%H:%M:%S")
+            numdays = (now - file_date).days
+            if numdays > range:
+                continue
+            # Download to tmp
+            local_path = configs['local_tmp'] + '/' + ifile
+            paths.append(f"{domain}|{local_path}")
+            logger.debug(f"Downloading ... domain: {domain} to {local_path}")
+            s3simple.download_file(file_name=ifile, output_file=local_path)
+
+    logger.debug(f"Paths: {paths}")
 
     for fpath in paths:
         if not fpath:
             continue
-        domain, path = fpath.split(':')
+        logger.debug(f"Path: {fpath}")
+        domain, path = fpath.split('|')
         if not os.path.exists(path):
             logger.critical("Path doesn't exist!")
             return
         if not os.path.isdir(path):
             files = [path]
         else:
-            files = get_list(path, recursive)
+            files = get_list(path, recursive, range)
+
+        logger.debug(f"File List: {files}")
 
         all_log_data = []
         for file_name in files:
@@ -60,13 +98,10 @@ def analyze(recursive, unzip, percent, num, daemon, skipsave, paths_ignore, just
                 (ext != 'bz2')): # not a log file, nor a zipped log file
                 continue
             
-            s3simple = S3Simple(region_name=configs['region'],
-                                profile=configs['profile'],
-                                bucket_name=configs['log_storage_bucket'])
             # send to S3
-            if not skipsave:
+            if not skipsave and not read_s3:
                 logger.debug("sending to s3...")
-                s3_file =  'raw_log_file-' + domain + '-' + just_file_name + '-' + now_string
+                s3_file =  'RawLogFile_' + domain + '_' + now_string + '_' + just_file_name
                 s3simple.send_file_to_s3(local_file=file_name, s3_file=s3_file)
 
             if not justsave:
@@ -92,21 +127,28 @@ def analyze(recursive, unzip, percent, num, daemon, skipsave, paths_ignore, just
                     print(output_text)
 
                 logger.debug("Saving log analysis file...")
-                key = 'log_analysis' + just_file_name + '-' + now_string
+                key = 'LogAnalysis_' + just_file_name + '_' + now_string + '.json'
                 body = str(analyzed_data)
                 s3simple.put_to_s3(key=key, body=body)
 
                 logger.debug("Saving output file....")
-                key = 'log_analysis_output-' + domain + '-' + now_string
+                key = 'LogAnalysisOutput_' + domain + '_' + now_string + '.txt'
                 s3simple.put_to_s3(key=key, body=output_text)
     
     return
 
-def get_list(path, recursive):
+def get_list(path, recursive, range):
+    now = datetime.datetime.now()
     file_list = os.listdir(path)
     all_files = []
     for entry in file_list:
         full_path = os.path.join(path, entry)
+        # How old is the file?
+        modified = datetime.datetime.fromtimestamp(os.path.getmtime(full_path))
+        numdays = (now - modified).days
+        logger.debug(f"File: {full_path} Age: {numdays}")
+        if numdays > range:
+            continue
         if os.path.isdir(full_path) and recursive:
             all_files = all_files + get_list(full_path, recursive)
         else:
