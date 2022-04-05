f0799eb7e95bcc355c96b46ce77f6468b766fd13
Iain Learmonth <irl@sr2.uk>
2022-04-05 16:07:49 +0100

    Auto replace blocked bridges

--- app/terraform/block_bridge_github.py
+++ app/terraform/block_bridge_github.py
@@ -1,20 +1,21 @@
 import datetime
+from time import timezone
 
 from dateutil.parser import isoparse
-from github import GitHub
+from github import Github
 
 from app import app
 from app.models import Bridge
 
 
 def check_blocks():
-    g = GitHub(app.config['GITHUB_API_KEY'])
+    g = Github(app.config['GITHUB_API_KEY'])
     repo = g.get_repo(app.config['GITHUB_BRIDGE_REPO'])
     for vp in app.config['GITHUB_BRIDGE_VANTAGE_POINTS']:
-        results = repo.get_contents(f"results_{vp}").splitlines()
+        results = repo.get_contents(f"recentResult_{vp}").decoded_content.decode('utf-8').splitlines()
         for result in results:
-            parts = result.split(" ")
-            if isoparse(parts[2]) < (datetime.datetime.utcnow() - datetime.timedelta(days=3)):
+            parts = result.split("\t")
+            if isoparse(parts[2]) < (datetime.datetime.now(timezone.utc) - datetime.timedelta(days=3)):
                 continue
             if int(parts[1]) < 40:
                 bridge = Bridge.query.filter(
